dfcfa12c30a4fc029dcb7cf53c270ef9ae7d8705
Anna Luiza dos Santos Barbosa <anna.luiza5212@gmail.com>
2025-07-16 15:41:15 -0300

    Add files via upload

---
+++ ex-senha.py
@@ -0,0 +1,20 @@
+
+while True:
+    nome = input("Informe seu nome: ")
+
+    senha = input("Informe sua senha de usuário: ")
+    
+    if senha == nome:
+        print("ERRO. A senha não poder ser igual ao nome de usuátio. Tente novamente! \n")
+        
+    else:
+        print("Cadastro realizado com sucesso!")
+        break
+  
+
+
+
+
+
+
+    
